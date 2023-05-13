81bb00ea41ee2cc68a1d4859142b0ad8bc42bade
Сергей <123937500+MartynHiro@users.noreply.github.com>
2023-05-13 16:19:22 +0700

    Server addded

---
+++ src/main/java/Client.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(Main.HOST, Main.PORT);
+             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.println("Пожалуйста введите слово для поиска");
+
+                //вводим и отправляем слово для поиска по pdf файлам
+                writer.println(scanner.nextLine());
+            }
+
+            //получаем в ответ json строку
+            System.out.println(reader.readLine());
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,49 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.itextpdf.io.IOException;
+
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.Arrays;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
 
 public class Main {
+    public static final String DIRECTORY_WITH_PDF = "pdfs";
+    public static final int PORT = 8989;
+    public static final String HOST = "127.0.0.1";
     public static void main(String[] args) throws Exception {
 
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
+        BooleanSearchEngine engine = new BooleanSearchEngine(new File(DIRECTORY_WITH_PDF));
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+
+            Gson gson = new GsonBuilder().create();
+
+            System.out.println("Сервер запустился");
+
+            while (true) {
+                try (Socket socket = serverSocket.accept();
+                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     PrintWriter writer = new PrintWriter(socket.getOutputStream())
+                ) {
+                    //читаем слово присланное клиентом
+                    List<PageEntry> searchResult = engine.search(reader.readLine());
+
+                    StringBuilder jsonAnswer = new StringBuilder();
+
+                    //собираем json строку ему в ответ
+                    searchResult.forEach(page -> jsonAnswer.append(gson.toJson(page)));
+
+                    writer.println(jsonAnswer);
+
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
     }
 }
